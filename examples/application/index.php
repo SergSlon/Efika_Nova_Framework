63f9a8cc4cf14ef63ef8a7e0416e7e7a4dfec91f
Marco Bunge <marco_bunge@web.de>
2013-03-10 23:20:45 +0100

    update application example

--- examples/application/index.php
+++ examples/application/index.php
@@ -4,13 +4,43 @@
  * @copyright 2012 Marco Bunge <efika@rubymatrix.de>
  */
 
+use Efika\Application\Application as WebApp;
+
 require_once '../entryPoint/bootstrap.php';
 
-class CustomService{
-    public function __construct(\Efika\Application\ApplicationInterface $app){
-        $app->attachEventHandler('onInit',new \Efika\EventManager\EventHandlerCallback(function(){
-            var_dump('Demacia!');
-        }));
+class CustomHandlerAggregate implements \Efika\EventManager\EventHandlerAggregateInterface
+{
+
+    public function onInit(\Efika\EventManager\EventInterface $e)
+    {
+        var_dump($e);
+    }
+
+    /**
+     * Attach events to parent event observer
+     *
+     * @param \Efika\EventManager\EventManagerTrait $parent
+     *
+     * @return mixed
+     */
+    public function attach($parent)
+    {
+        $aggregate = $this;
+        $parent->attachEventHandler(WebApp::ON_INIT,function($e) use ($aggregate){
+            $aggregate->onInit($e);
+        });
+    }
+
+    /**
+     * Detach events from parent event observer
+     *
+     * @param $parent
+     *
+     * @return mixed
+     */
+    public function detach($parent)
+    {
+        // TODO: Implement detach() method.
     }
 }
 
@@ -18,10 +48,11 @@ $config = [
     'ConfigVar' => 'foo.bar',
 ];
 
-$app = Efika\Application\Application::getInstance();
+
+$app = WebApp::getInstance();
 $app->configure($config);
 
-new CustomService($app);
+$app->attachEventHandlerAggregate(new CustomHandlerAggregate());
 
 var_dump($app->execute());
 
