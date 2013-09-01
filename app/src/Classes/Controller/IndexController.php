b2a7fe13d9e254e77e5a26c2fb78d7492f775856
mbunge <marco.bunge@rubymatrix.de>
2013-09-01 21:09:03 +0200

    add defaultcontroller

---
+++ app/src/Classes/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Marco Bunge
+ * @copyright 2012 Marco Bunge <efika@rubymatrix.de>
+ */
+
+namespace WebApplication\Controller;
+
+
+use Efika\Application\Commands\ControllerCommand;
+use Efika\Http\Response\HttpContent;
+
+class IndexController extends ControllerCommand{
+
+    public function indexAction(){
+//        return new HttpContent(['Hello World',"\n",'Welcome!']);
+        return 'hello world!';
+    }
+
+}
